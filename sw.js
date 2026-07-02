dccef315511f70bb37e1a83c3593f38552a4f594
fifeetch <45316977+fifeetch@users.noreply.github.com>
2026-07-02 08:48:29 +0200

    Placer les paramètres en haut du menu mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "cap-montagne-v23";
-const APP_SHELL = ["./", "./index.html", "./styles.css?v=23", "./app.js?v=23", "./firebase-config.js?v=2", "./manifest.json", "./icon.svg"];
+const CACHE_NAME = "cap-montagne-v24";
+const APP_SHELL = ["./", "./index.html", "./styles.css?v=24", "./app.js?v=24", "./firebase-config.js?v=2", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
