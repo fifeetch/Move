424f80dabf01fc38ff23225d99275619d52f1610
fifeetch <45316977+fifeetch@users.noreply.github.com>
2026-07-02 08:36:15 +0200

    Déplacer les paramètres dans le menu mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "cap-montagne-v21";
-const APP_SHELL = ["./", "./index.html", "./styles.css?v=21", "./app.js?v=21", "./firebase-config.js?v=2", "./manifest.json", "./icon.svg"];
+const CACHE_NAME = "cap-montagne-v22";
+const APP_SHELL = ["./", "./index.html", "./styles.css?v=22", "./app.js?v=22", "./firebase-config.js?v=2", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
