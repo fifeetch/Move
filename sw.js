9440a569957c5ecbf6749553b8df1edef75c3674
fifeetch <45316977+fifeetch@users.noreply.github.com>
2026-07-02 08:23:49 +0200

    Renforcer les modes d'affichage et la taille globale

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "cap-montagne-v19";
-const APP_SHELL = ["./", "./index.html", "./styles.css?v=19", "./app.js?v=19", "./firebase-config.js?v=2", "./manifest.json", "./icon.svg"];
+const CACHE_NAME = "cap-montagne-v20";
+const APP_SHELL = ["./", "./index.html", "./styles.css?v=20", "./app.js?v=20", "./firebase-config.js?v=2", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
